529314edb3097aa23a2cb7c689a7b56bf73265d0
Adam Kowalczyk <kombajno678@gmail.com>
2022-04-06 19:23:33 +0200

    script to deploy and verify contract at multiple networks at once

---
+++ sample_contract/scripts/deploy_all_networks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+network_names=(rinkeby mumbai_moralis bsc-test)
+for i in "${network_names[@]}"; do
+    echo "Deploying to $i ..."
+    brownie run scripts/passwordless/deploy.py --network $i
+    echo "Verifying contract at $i ..."
+    brownie run scripts/passwordless/verify.py --network $i
+done
