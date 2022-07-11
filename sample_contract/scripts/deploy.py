fae72ebc21d921a29b2af56ad4112e881283550f
Adam Kowalczyk <kombajno678@gmail.com>
2022-07-11 16:04:53 +0200

    formatting

--- sample_contract/scripts/deploy.py
+++ sample_contract/scripts/deploy.py
@@ -1,11 +1,11 @@
-
 from brownie import AuthenticatorProvider
 from scripts.utils import get_account
 
 
 def deploy():
     account = get_account()
-    authenticatorProvider = AuthenticatorProvider.deploy({"from": account})
+    authenticatorProvider = AuthenticatorProvider.deploy(
+        {"from": account})
     print(
         f"AuthenticatorProvider has been deployed at address: {authenticatorProvider.address}")
     return authenticatorProvider
--- sample_contract/scripts/passwordless/deploy.py
+++ sample_contract/scripts/passwordless/deploy.py
@@ -5,7 +5,8 @@ from scripts.utils import get_account
 
 def deploy():
     account = get_account()
-    contract = PasswordlessAuthentication.deploy({"from": account})
+    contract = PasswordlessAuthentication.deploy(
+        {"from": account, "allow_revert": True, "gas_limit": 2_000_000})
     print(
         f"PasswordlessAuthentication has been deployed at address: {contract.address}")
     return contract
