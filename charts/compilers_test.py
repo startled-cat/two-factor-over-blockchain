c55e570b99e74d311f977897e038848db470ddff
Adam Kowalczyk <kombajno678@gmail.com>
2022-07-18 19:14:23 +0200

    charts update

--- charts/compilers_test.py
+++ charts/compilers_test.py
@@ -3,56 +3,149 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-with open('../data/compilers_test.json') as f:
-    stats = json.load(f)
 
-    # filter data
+def load_data(filename):
+    with open(filename) as f:
+        data = json.load(f)
+    return data
 
-    stats = list(filter(lambda x: x['solcVersion'] == "0.8.15", stats))
-    # stats = list(filter(lambda x: x['solcOptimizerRuns'] == 0, stats))
+
+def chart_optimization_runs(data_path=None, show=False, save_as=None):
+
+    # stats = load_data("../data/compilers_test_old.json")
+    # stats = list(filter(lambda x: x["solcVersion"] == "0.8.15", stats))
+    stats = load_data(data_path)
+    stats = list(filter(lambda x: x["solcOptimizerRuns"] is not None, stats))
+
+    solc_version = stats[0]["solcVersion"]
 
     # prepare data
-
     x = [i for i in range(0, len(stats))]
-    userGasUsed = [s["userGasUsed"] for s in stats]
-    appGasUsed = [s["appGasUsed"] for s in stats]
-    solcVersion = [s["solcVersion"] for s in stats]
-    solcOptimizerRuns = [s["solcOptimizerRuns"] for s in stats]
-    solcOptimizerRuns = list(
-        map(lambda x: "none" if x is None else x, solcOptimizerRuns))
-    contractDeployGasUsed = [s["contractDeployGasUsed"] for s in stats]
-
-    totalGasUsed = [s["userGasUsed"]+s["appGasUsed"] for s in stats]
-
-    # prepare chart
-    fig, ax1 = plt.subplots()
-    plt.xticks(x, solcOptimizerRuns, rotation=45)
-
-    color = 'tab:red'
-    ax1.set_xlabel('compiler optimization runs')
-    ax1.set_ylabel('transaction gas used', color=color)
-    ax1.plot(x, totalGasUsed, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
+    solc_optimizer_runs = ["Brak" if s["solcOptimizerRuns"]
+                           is None else s["solcOptimizerRuns"] for s in stats]
+    deployment_gas_used = [s["contractDeployGasUsed"] for s in stats]
+    tx_gas_used = [s["userGasUsed"]+s["appGasUsed"] for s in stats]
 
     def formatter(x, pos=None):
-        s = solcOptimizerRuns[x]
-        if isinstance(s, str):
-            return s
+        return f"{solc_optimizer_runs[math.floor(x)]:,}".replace(",", " ")
 
-        return f"{solcOptimizerRuns[x]:,}"
-    ax1.xaxis.set_major_formatter(formatter)
-
-    ax2 = ax1.twinx()
-    color = 'tab:blue'
-    ax2.set_ylabel('contract deployment gas used', color=color)
-    ax2.plot(x, contractDeployGasUsed, color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
+    # prepare chart
+    plt.rcParams["figure.figsize"] = (8, 5)
+    fig, ax1 = plt.subplots()
 
     fig.tight_layout()
+    plt.xticks(x, solc_optimizer_runs, rotation=90)
+    plt.subplots_adjust(top=0.9, left=0.15, right=0.85, bottom=0.25)
 
-    plt.title('usage of gas for transactions, depending of compiler settings')
+    color1 = "tab:red"
+    color2 = "tab:blue"
+    label1 = "gas na wykonanie transakcji"
+    label2 = "gas na wdrożenie kontraktu"
 
-    plt.legend()
-    plt.show()
-    # plt.savefig('compilers.test.svg', dpi=350)
-    # plt.show()
+    ax1.set_xlabel(
+        f"Wartość parametru \"Optimization runs\"")
+    ax1.set_ylabel(label1, color=color1)
+    line1, = ax1.plot(x, tx_gas_used, color=color1,
+                      label=label1, marker="s", linestyle="dashed")
+    ax1.tick_params(axis="y", labelcolor=color1)
+    ax1.xaxis.set_major_formatter(formatter)
+    ax1.grid(which="major", alpha=0.7, axis="x")
+    ax1.grid(which="major", alpha=0.7, axis="y")
+    ax1.yaxis.set_major_formatter(
+        lambda x, pos=None: f"{x:,}"[:-2].replace(",", " "))
+    ax2 = ax1.twinx()
+    ax2.set_ylabel(label2, color=color2)
+    line2, = ax2.plot(x, deployment_gas_used, color=color2,
+                      label=label2, marker="o")
+    ax2.tick_params(axis="y", labelcolor=color2)
+    ax2.yaxis.set_major_formatter(
+        lambda x, pos=None: f"{x:,}"[:-2].replace(",", " "))
+
+    plt.title(
+        f"Zapotrzebowanie na gas, zależnie od parametru \"Optimization runs\" \ndla kompilatora solc w wersji {solc_version}")
+
+    plt.legend([line1, line2], [label1, label2], loc='center right')
+
+    if show:
+        plt.show()
+
+    if save_as is not None:
+        plt.savefig(save_as, dpi=350)
+
+
+def chart_compiler_versions(show=False, save_as=None):
+    stats = load_data("../data/compare_compiler_versions.json")
+
+    optimization_runs = stats[0]["solcOptimizerRuns"]
+    # prepare data
+    x = [i for i in range(0, len(stats))]
+    solc_version = [s["solcVersion"] for s in stats]
+    deployment_gas_used = [s["contractDeployGasUsed"] for s in stats]
+    tx_gas_used = [s["userGasUsed"]+s["appGasUsed"] for s in stats]
+
+    # prepare chart
+    plt.rcParams["figure.figsize"] = (14, 4)
+
+    fig, ax1 = plt.subplots()
+    fig.tight_layout()
+    plt.xticks(x, solc_version, rotation=90)
+    plt.subplots_adjust(top=0.9, left=0.08, right=0.9, bottom=0.25)
+
+    color1 = "tab:red"
+    label1 = "gas na wykonanie transakcji"
+    ax1.set_xlabel(
+        f"Wersja kompilatora Solidity")
+    line1, = ax1.plot(x, tx_gas_used, color=color1, linestyle="dashed",
+                      label=label1, marker="s")
+    ax1.set_ylabel(label1, color=color1)
+    ax1.tick_params(axis="y", labelcolor=color1)
+
+    ax2 = ax1.twinx()
+    color2 = "tab:blue"
+    label2 = "gas na wdrożenie kontraktu"
+    ax2.set_ylabel(label2, color=color2)
+    line2, = ax2.plot(x, deployment_gas_used, color=color2,
+                      label=label2, marker="o")
+    ax2.tick_params(axis="y", labelcolor=color2)
+
+    # add grid to chart
+    # ax1.grid(which="major")
+    ax1.grid(which="major", alpha=0.7, axis="x")
+    ax1.grid(which="major", alpha=0.7, axis="y")
+
+    ax1.xaxis.set_major_formatter(lambda x, pos=None: solc_version[round(x)])
+    ax1.yaxis.set_major_formatter(
+        lambda x, pos=None: f"{x:,}"[:-2].replace(",", " "))
+    ax2.yaxis.set_major_formatter(
+        lambda x, pos=None: f"{x:,}"[:-2].replace(",", " "))
+
+    plt.title(
+        f"Zapotrzebowanie na gas, zależne od wersji kompilatora solc (\"Optimization runs\" : {optimization_runs})")
+
+    ax1.legend([line1, line2], [label1, label2], loc='center left')
+
+    if show:
+        plt.show()
+
+    if save_as is not None:
+        plt.savefig(save_as, dpi=350)
+
+
+def main():
+    chart_optimization_runs(
+        data_path="../data/compare_compiler_optimizations_1.json",
+        show=False,
+        save_as="images/compilers_optimization_1.svg")
+
+    chart_optimization_runs(
+        data_path="../data/compare_compiler_optimizations_2.json",
+        show=False,
+        save_as="images/compilers_optimization_2.svg")
+
+    chart_compiler_versions(
+        show=False,
+        save_as="images/compilers_versions.svg")
+
+
+if __name__ == "__main__":
+    main()
--- passwordless_auth/scripts/benchmark/compile_test.py
+++ passwordless_auth/scripts/benchmark/compile_test.py
@@ -3,6 +3,111 @@ from brownie import PasswordlessAuthentication
 from scripts.utils import get_account, measure_contract_stats
 from brownie import compile_source, web3
 import json
+import numpy as np
+
+solcVersions = [
+    "0.8.15",
+    "0.8.14",
+    "0.8.13",
+    "0.8.12",
+    "0.8.11",
+    "0.8.10",
+    "0.8.9",
+    "0.8.8",
+    "0.8.7",
+    "0.8.6",
+    "0.8.5",
+    "0.8.4",
+    "0.8.3",
+    "0.8.2",
+    "0.8.1",
+    "0.8.0",
+    "0.7.6",
+    "0.7.5",
+    "0.7.4",
+    "0.7.3",
+    "0.7.2",
+    "0.7.1",
+    "0.7.0",
+    "0.6.12",
+    "0.6.11",
+    "0.6.10",
+    "0.6.9",
+    "0.6.8",
+    "0.6.7",
+    "0.6.6",
+    "0.6.5",
+    "0.6.4",
+    "0.6.3",
+    "0.6.2",
+    "0.6.1",
+    "0.6.0",
+    "0.5.17",
+    "0.5.16",
+    "0.5.15",
+    "0.5.14",
+    "0.5.13",
+    "0.5.12",
+    "0.5.11",
+    "0.5.10",
+    "0.5.9",
+    "0.5.8",
+    "0.5.7",
+    "0.5.6",
+    "0.5.5",
+    "0.5.4",
+    "0.5.3",
+    "0.5.2",
+    "0.5.1",
+    "0.5.0",
+    "0.4.26",
+    "0.4.25",
+    "0.4.24",
+    "0.4.23",
+    "0.4.22",
+    "0.4.21",
+    "0.4.20",
+    "0.4.19",
+    "0.4.18",
+    "0.4.17",
+    "0.4.16",
+    "0.4.15",
+    "0.4.14",
+    "0.4.13",
+    "0.4.12",
+    "0.4.11",
+    "0.4.10",
+    "0.4.9",
+    "0.4.8",
+    "0.4.7",
+    "0.4.6",
+    "0.4.5",
+    "0.4.4",
+    "0.4.3",
+    "0.4.2",
+    "0.4.1",
+    "0.4.0",
+    "0.3.6",
+    "0.3.5",
+    "0.3.4",
+    "0.3.3",
+    "0.3.2",
+    "0.3.1",
+    "0.3.0",
+    "0.2.2",
+    "0.2.1",
+    "0.2.0",
+    "0.1.7",
+    "0.1.6",
+    "0.1.5",
+    "0.1.4",
+    "0.1.3",
+    "0.1.2"
+]
+
+optimizeRuns = [*range(100, 4_001, 200)]
+optimizeRunsLog = [None, 0, 1, 10, 100, 1_000,
+                   10_000, 100_000, 1_000_000, 10_000_000]
 
 
 def compile_from_source(source, settings):
@@ -19,114 +124,80 @@ def compile_from_source(source, settings):
     return contract
 
 
-def compare_compiler_settings():
-    user1 = get_account(0)
-    app1 = get_account(1)
+# def compare_compiler_settings():
+#     user1 = get_account(0)
+#     app1 = get_account(1)
+#     contractSourceCode = PasswordlessAuthentication._sources.get(
+#         "PasswordlessAuthentication")
+#     # https://eth-brownie.readthedocs.io/en/stable/api-project.html?highlight=compile#main.compile_source
+
+#     # brownie supports solc versions from 0.4.22
+#     brownieSolcVersions = solcVersions[:57]
+#     compilerSettings = {
+#         "solc_version": "0.8.15",
+#         "optimize": True,
+#         "runs": 200,
+#         "evm_version": None,
+#     }
+
+#     stats = []
+
+#     for solcVersion in brownieSolcVersions:
+#         print(f"Testing solc version: {solcVersion}")
+#         compilerSettings["solc_version"] = solcVersion
+#         for runs in optimizeRuns:
+#             print(f"Testing runs: {runs}")
+#             compilerSettings["runs"] = runs
+#             compilerSettings["optimize"] = True if runs is not None else False
+#             contract = compile_from_source(
+#                 contractSourceCode, compilerSettings)
+
+#             s = measure_contract_stats(contract, n=1)
+#             s.pop("time")
+#             s.pop("userGasPrice")
+#             s.pop("appGasPrice")
+#             s["solcVersion"] = solcVersion
+#             s["solcOptimizerRuns"] = runs
+#             s["optimize"] = compilerSettings["optimize"]
+#             s["contractDeployGasUsed"] = contract.tx.gas_used
+#             stats.append(s)
+#     return stats
+
+
+def compare_compiler_versions(runs=10_000):
+    contractSourceCode = PasswordlessAuthentication._sources.get(
+        "PasswordlessAuthentication")
+    # brownie supports solc versions from 0.4.22
+    brownieSolcVersions = solcVersions[:57]
+    compilerSettings = {
+        "solc_version": "0.8.15",
+        "optimize": True,
+        "runs": runs,
+        "evm_version": None,
+    }
+
+    stats = []
+
+    for solcVersion in brownieSolcVersions:
+        compilerSettings["solc_version"] = solcVersion
+        contract = compile_from_source(contractSourceCode, compilerSettings)
+        s = measure_contract_stats(contract, n=1)
+        s.pop("time")
+        s.pop("userGasPrice")
+        s.pop("appGasPrice")
+        s["solcVersion"] = solcVersion
+        s["solcOptimizerRuns"] = compilerSettings["runs"]
+        s["optimize"] = compilerSettings["optimize"]
+        s["contractDeployGasUsed"] = contract.tx.gas_used
+        stats.append(s)
+    return stats
+
+
+def compare_compiler_optimizations(solcVersion="0.8.15", runs_list=optimizeRunsLog):
     contractSourceCode = PasswordlessAuthentication._sources.get(
         "PasswordlessAuthentication")
     # https://eth-brownie.readthedocs.io/en/stable/api-project.html?highlight=compile#main.compile_source
 
-    solcVersions = [
-        "0.8.15",
-        "0.8.14",
-        "0.8.13",
-        "0.8.12",
-        "0.8.11",
-        "0.8.10",
-        "0.8.9",
-        "0.8.8",
-        "0.8.7",
-        "0.8.6",
-        "0.8.5",
-        "0.8.4",
-        "0.8.3",
-        "0.8.2",
-        "0.8.1",
-        "0.8.0",
-        "0.7.6",
-        "0.7.5",
-        "0.7.4",
-        "0.7.3",
-        "0.7.2",
-        "0.7.1",
-        "0.7.0",
-        "0.6.12",
-        "0.6.11",
-        "0.6.10",
-        "0.6.9",
-        "0.6.8",
-        "0.6.7",
-        "0.6.6",
-        "0.6.5",
-        "0.6.4",
-        "0.6.3",
-        "0.6.2",
-        "0.6.1",
-        "0.6.0",
-        "0.5.17",
-        "0.5.16",
-        "0.5.15",
-        "0.5.14",
-        "0.5.13",
-        "0.5.12",
-        "0.5.11",
-        "0.5.10",
-        "0.5.9",
-        "0.5.8",
-        "0.5.7",
-        "0.5.6",
-        "0.5.5",
-        "0.5.4",
-        "0.5.3",
-        "0.5.2",
-        "0.5.1",
-        "0.5.0",
-        "0.4.26",
-        "0.4.25",
-        "0.4.24",
-        "0.4.23",
-        "0.4.22",
-        "0.4.21",
-        "0.4.20",
-        "0.4.19",
-        "0.4.18",
-        "0.4.17",
-        "0.4.16",
-        "0.4.15",
-        "0.4.14",
-        "0.4.13",
-        "0.4.12",
-        "0.4.11",
-        "0.4.10",
-        "0.4.9",
-        "0.4.8",
-        "0.4.7",
-        "0.4.6",
-        "0.4.5",
-        "0.4.4",
-        "0.4.3",
-        "0.4.2",
-        "0.4.1",
-        "0.4.0",
-        "0.3.6",
-        "0.3.5",
-        "0.3.4",
-        "0.3.3",
-        "0.3.2",
-        "0.3.1",
-        "0.3.0",
-        "0.2.2",
-        "0.2.1",
-        "0.2.0",
-        "0.1.7",
-        "0.1.6",
-        "0.1.5",
-        "0.1.4",
-        "0.1.3",
-        "0.1.2"
-    ]
-    # brownie supports solc versions from 0.4.22
-    brownieSolcVersions = solcVersions[:57]
     compilerSettings = {
         "solc_version": "0.8.15",
         "optimize": True,
@@ -134,35 +205,38 @@ def compare_compiler_settings():
         "evm_version": None,
     }
 
-    optimizeRuns = [None, 0, 1, 10, 50, 100, 200, 400, 600, 800, 1_000, 2_000,
-                    5_000, 10_000, 50_000, 100_000, 200_000, 500_000, 1_000_000, 10_000_000]
-
     stats = []
+    compilerSettings["solc_version"] = solcVersion
+    for runs in runs_list:
+        compilerSettings["runs"] = runs
+        compilerSettings["optimize"] = True if runs is not None else False
+        contract = compile_from_source(
+            contractSourceCode, compilerSettings)
 
-    for solcVersion in brownieSolcVersions:
-        print(f"Testing solc version: {solcVersion}")
-        compilerSettings["solc_version"] = solcVersion
-        for runs in optimizeRuns:
-            print(f"Testing runs: {runs}")
-            compilerSettings["runs"] = runs
-            compilerSettings["optimize"] = True if runs is not None else False
-            contract = compile_from_source(
-                contractSourceCode, compilerSettings)
-
-            s = measure_contract_stats(contract, n=1)
-            s.pop("time")
-            s.pop("userGasPrice")
-            s.pop("appGasPrice")
-            s["solcVersion"] = solcVersion
-            s["solcOptimizerRuns"] = runs
-            s["optimize"] = compilerSettings["optimize"]
-            s["contractDeployGasUsed"] = contract.tx.gas_used
-            stats.append(s)
+        s = measure_contract_stats(contract, n=1)
+        s.pop("time")
+        s.pop("userGasPrice")
+        s.pop("appGasPrice")
+        s["solcVersion"] = compilerSettings["solc_version"]
+        s["solcOptimizerRuns"] = compilerSettings["runs"]
+        s["optimize"] = compilerSettings["optimize"]
+        s["contractDeployGasUsed"] = contract.tx.gas_used
+        stats.append(s)
     return stats
 
 
 def main():
-    stats = compare_compiler_settings()
 
-    with open('../data/compilers_test.json', 'w') as f:
+    # print("compare_compiler_versions ...")
+    # stats = compare_compiler_versions()
+    # with open('../data/compare_compiler_versions.json', 'w') as f:
+    #     json.dump(stats, f)
+
+    print("compare_compiler_optimizations ...")
+    stats = compare_compiler_optimizations("0.8.15", runs_list=optimizeRunsLog)
+    with open('../data/compare_compiler_optimizations_1.json', 'w') as f:
+        json.dump(stats, f)
+
+    stats = compare_compiler_optimizations("0.8.15", runs_list=optimizeRuns)
+    with open('../data/compare_compiler_optimizations_2.json', 'w') as f:
         json.dump(stats, f)
