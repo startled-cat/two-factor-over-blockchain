fd39b8ff896c8b7cb8a54fca53f741ad34e5ba31
Adam Kowalczyk <kombajno678@gmail.com>
2022-07-25 18:22:42 +0200

    verbose deploy script

--- passwordless_auth/scripts/deploy.py
+++ passwordless_auth/scripts/deploy.py
@@ -1,14 +1,21 @@
 
-from brownie import PasswordlessAuthentication
+from brownie import PasswordlessAuthentication, network, Wei
 from scripts.utils import get_account
 
 
 def deploy():
     account = get_account()
+    user_balance = account.balance()
+    print(f'user1: {account.address}')
+    print(f'    { Wei(user_balance).to("ether") } eth')
+    print(f'    { Wei(user_balance).to("gwei") } gwei')
+    print(f'    {     (user_balance) } wei')
+    
+    
     contract = PasswordlessAuthentication.deploy(
-        {"from": account, "allow_revert": True, "gas_limit": 2_000_000})
+        {"from": account, "allow_revert": False, "gas_limit": 10_000_000})
     print(
-        f"PasswordlessAuthentication has been deployed at address: {contract.address}")
+        f"PasswordlessAuthentication has been deployed at {network.show_active()} address: {contract.address}")
     return contract
 
 
