74e0a5a5b05c7f4f6695be28ce98e09d5b391b3e
Adam Kowalczyk <kombajno678@gmail.com>
2022-07-25 18:22:58 +0200

    improved banchmark script

--- passwordless_auth/scripts/benchmark/test_passwordless_performance.py
+++ passwordless_auth/scripts/benchmark/test_passwordless_performance.py
@@ -1,4 +1,4 @@
-from scripts.utils import get_account, get_formatted_timestamp, measure_contract_stats, is_network_local
+from scripts.utils import get_account, get_formatted_timestamp, log_prefix, measure_contract_stats, is_network_local
 from brownie import PasswordlessAuthentication, network, accounts, config, chain
 from scripts.deploy import deploy
 import threading
@@ -7,6 +7,7 @@ import time
 import json
 from tinydb import TinyDB, Query
 from datetime import datetime
+
 
 class LocalChainMineThread(threading.Thread):
     def __init__(self):
@@ -32,6 +33,7 @@ def execute_single_test(contract, db_path):
     try:
         stats = measure_contract_stats(
             contract, n=1, confirmations=5)
+        print(f"{log_prefix()} writing stats to {db_path}")
         with TinyDB(db_path) as db:
             for s in stats:
                 db.insert(s)
@@ -40,19 +42,20 @@ def execute_single_test(contract, db_path):
 
 
 def test_speed():
-    
-    runs_per_h = 3
+
+    runs_per_h = 4
     total_test_time = 12
-    
+
     total_runs = runs_per_h * total_test_time
     sleep_between_runs = 3600 / runs_per_h
-    
+
     # total_runs = 1
-    # sleep_between_runs = 0
-    
+    # sleep_between_runs = 1
+
     if len(PasswordlessAuthentication) < 1:
         deploy()
     contract = PasswordlessAuthentication[-1]
+    print(f"{log_prefix()} contract: {contract.address}")
 
     if is_network_local():
         thread = LocalChainMineThread()
@@ -60,32 +63,51 @@ def test_speed():
     else:
         accounts.add(config["wallets"]["from_key"])
         accounts.add(config["wallets"]["from_key2"])
+        print(f'accounts:')
+        for a in accounts:
+            print(f'    {a.address}')
 
-    db_path = f'../data/benchmark/{network.show_active()}_{total_runs}_{datetime.now().strftime("%y%m%d-%H%M")}.json'
+    db_path = f'/home/adamko-wsl/git/two-factor-over-blockchain/data/benchmark/network/{network.show_active()}/{network.show_active()}_{total_runs}_{datetime.now().strftime("%y%m%d-%H%M")}.json'
     with TinyDB(db_path) as db:
         db.drop_tables()
         db.close()
 
     test_thread = None
     for _ in range(0, total_runs):
-        print("============================================================================")
-        print("============================================================================")
+        print(
+            "============================================================================")
+        print(
+            "============================================================================")
         print(f"        Current datetime : {get_formatted_timestamp()}")
-        print(f"        Running test no: {_+1}/{total_runs}; tests per hour: {runs_per_h}, total test time: {total_test_time}")
-        print("============================================================================")
-        print("============================================================================")
-        
+        print(
+            f"        Running test no: {_+1}/{total_runs}; tests per hour: {runs_per_h}, total test time: {total_test_time}")
+        print(
+            "============================================================================")
+        print(
+            "============================================================================")
+
         test_thread = threading.Thread(target=execute_single_test,
                                        args=(contract, db_path))
+        print(f"{log_prefix()} Starting test thread")
         test_thread.start()
+        print(f"{log_prefix()} Sleeping for {sleep_between_runs} seconds")
         time.sleep(sleep_between_runs)
+        # join thread
+        if test_thread.is_alive():
+            print(f"{log_prefix()} Joining test thread")
+            test_thread.join()
 
     if is_network_local():
         test_thread.join()
         time.sleep(10)
         thread.stop()
         thread.join()
-
+        
+    with TinyDB(db_path) as db:
+        measurements = len(db.all())
+        print(f"{log_prefix()} saved measurements: {measurements}")
+    
+    print(f"{log_prefix()} Exiting main thread")
 
 def main():
     test_speed()
--- passwordless_auth/scripts/utils.py
+++ passwordless_auth/scripts/utils.py
@@ -1,3 +1,5 @@
+import random
+from brownie import Wei
 from datetime import datetime
 from brownie import network, accounts, config, chain
 from brownie.network.gas.strategies import LinearScalingStrategy, ExponentialScalingStrategy
@@ -5,8 +7,13 @@ import time
 LOCAL_BLOCKCHAIN_NETWORKS = ["development", "ganache-local"]
 FORKED_LOCAL_NETWORKS = ["mainnet-fork", "mainnet-fork-dev"]
 
+
 def get_formatted_timestamp():
     return datetime.now().strftime('%y-%m-%d %H:%M:%S')
+
+
+def log_prefix():
+    return f'[INFO][{get_formatted_timestamp()}] > '
 
 
 def is_network_local():
@@ -25,16 +32,27 @@ def get_account(index=None, id=None):
     return acc
 
 
-def measure_contract_stats(contract, n=3, confirmations=1):
+def measure_contract_stats(contract, n=1, confirmations=5):
     if is_network_local():
         user1 = get_account(0)
         app1 = get_account(1)
     else:
-        user1 = get_account(index=2)
-        app1 = get_account(index=1)
+        available_accounts_indexes = [-2, -1]
+        # randomize the accounts order
+        random.shuffle(available_accounts_indexes)
+        user1 = get_account(index=available_accounts_indexes[0])
+        app1 = get_account(index=available_accounts_indexes[1])
 
+    user_balance = user1.balance()
     print(f'user1: {user1.address}')
+    print(f'    { Wei(user_balance).to("ether") } eth')
+    print(f'    { Wei(user_balance).to("gwei") } gwei')
+    print(f'    {     (user_balance) } wei')
+    app_balance = app1.balance()
     print(f'app1: {app1.address}')
+    print(f'    {Wei(app_balance).to("ether") } eth')
+    print(f'    {Wei(app_balance).to("gwei") } gwei')
+    print(f'    {   (app_balance) } wei')
 
     # network.gas_price("auto")
 
@@ -47,17 +65,18 @@ def measure_contract_stats(contract, n=3, confirmations=1):
 
     stats = []
     for _ in range(0, n):
-
+        print(f"{log_prefix()} measuring contract stats..., test {_+1}/{n}")
         x = {
             "network": network.show_active(),
             "confirmations": confirmations,
 
             "timestamp": time.time(),
-            
+            "datetime": datetime.now().isoformat(),
+
             "fee": network.priority_fee(),
 
-            "user_hash":None,
-            "user_timestamp":None,
+            "user_hash": None,
+            "user_timestamp": None,
             "user_time": None,
             "user_tx_status": None,
             "user_gas_used": None,
@@ -65,8 +84,8 @@ def measure_contract_stats(contract, n=3, confirmations=1):
             "user_cost": None,
             "user_error": None,
 
-            "app_hash":None,
-            "app_timestamp":None,
+            "app_hash": None,
+            "app_timestamp": None,
             "app_time": None,
             "app_tx_status": None,
             "app_gas_used": None,
@@ -74,7 +93,7 @@ def measure_contract_stats(contract, n=3, confirmations=1):
             "app_cost": None,
             "app_error": None,
         }
-        
+
         def fill(confirmation_times, tx, prefix):
             x[prefix+"_hash"] = tx.txid
             x[prefix+"_timestamp"] = tx.timestamp
@@ -86,7 +105,11 @@ def measure_contract_stats(contract, n=3, confirmations=1):
 
         try:
             confirmation_times, tx = execute_contract_function(
-                contract, "giveAccess", {"from": user1, "gas_limit": 75_000}, args=[app1, 3200], confirmations=confirmations, retires=5)
+                contract, "giveAccess",
+                {"from": user1, "gas_limit": 1_000_000, "required_confs": 0},
+                args=[app1, 3200],
+                confirmations=confirmations,
+                retires=8)
             fill(confirmation_times, tx, "user")
         except Exception as e:
             x["user_tx_status"] = -2
@@ -94,10 +117,25 @@ def measure_contract_stats(contract, n=3, confirmations=1):
             print(e)
         finally:
             pass
+        
+        
+        
+        while True:
+            check_access_result = contract.checkAccess(user1, app1, {"from": app1})
+            print(f"{log_prefix()} check_access_result: {check_access_result}")
+            if check_access_result:
+                break
+            else:
+                time.sleep(1)
+        
 
         try:
             confirmation_times, tx = execute_contract_function(
-                contract, "receiveAccess", {"from": app1, "gas_limit": 35_000}, args=[user1], confirmations=confirmations, retires=5)
+                contract, "receiveAccess",
+                {"from": app1, "gas_limit": 1_000_000, "required_confs": 0},
+                args=[user1],
+                confirmations=confirmations,
+                retires=10)
             fill(confirmation_times, tx, "app")
         except Exception as e:
             x["app_tx_status"] = -2
@@ -107,29 +145,45 @@ def measure_contract_stats(contract, n=3, confirmations=1):
             pass
 
         stats.append(x)
+        
+        print(f"{log_prefix()} user balance enough for : {user_balance / x['user_cost']} transactions")
+        print(f"{log_prefix()} app balance enough for  : {app_balance / x['app_cost']} transactions")
 
     return stats
 
 
-def execute_contract_function(contract, function_name, tx_params, args=[], confirmations=1, retires=0):
+def execute_contract_function(contract, function_name, tx_params, args=[], confirmations=1, retires=0, retry_sleep=10):
 
     try:
         confirmation_times = []
         start_time = time.time()
+        print(f"{log_prefix()} executing '{function_name}' with args: {args}")
         tx = getattr(contract, function_name)(*args, tx_params)
 
         for c in range(1, confirmations+1):
-            print(f"{get_formatted_timestamp()} > waiting for {c} confirmations...")
-            tx.wait(c)
+            print(f"{log_prefix()} waiting for {c} confirmations...")
+
+            # tx.wait(c)
+            waited_for = 0
+            while tx.confirmations < c:
+                time.sleep(0.1)
+                waited_for += 0.1
+                if waited_for > 300:
+                    print(
+                        f"{log_prefix()} timed out while waiting for {c} confirmations")
+                    break
+
             confirmation_times.append(time.time() - start_time)
-            print("last confirmations time: ", confirmation_times[-1])
+            print(f"{log_prefix()} last confirmations time: ",
+                  confirmation_times[-1])
 
         return confirmation_times, tx
     except Exception as e:
-        print(f"{get_formatted_timestamp()} > Transaction failed:", e)
+        print(f"{log_prefix()} Transaction failed:", e)
         if retires > 0:
-            print(f"{get_formatted_timestamp()} > Retrying in 10s...(retries left:", retires, ")")
-            time.sleep(10)
-            return execute_contract_function(contract, function_name, tx_params, args, confirmations, retires-1)
+            print(
+                f"{log_prefix()} Retrying in {retry_sleep} s...(retries left:", retires, ")")
+            time.sleep(retry_sleep)
+            return execute_contract_function(contract, function_name, tx_params, args, confirmations, retires-1, retry_sleep=retry_sleep*1.5)
         else:
             raise e
